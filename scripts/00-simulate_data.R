692502d46f8dbc4bb99b94a1ae1afc3b3d415d7f
Jessica <jessicajwooim@gmail.com>
2024-02-07 18:14:08 -0500

    Creating simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,9 +1,55 @@
 #### Preamble ####
 # Purpose: 
 # Author:
+# Date:
+# Contact:
+# License:
+# Pre-requisites:
+
+
+#### Workspace Setup ####
+library(tidyverse)
+
+
+#### Build Simulated Data ####
+set.seed(250)
+
+
+# Simulate
+simulated_questionnaire_responses <-
+  tibble(
+    participant_number =
+      rep(c(1:200)),
+    
+    country = 
+      sample(
+        x = c("Denmark", "Sweden", "U.S.", "U.K."),
+        size = 200,
+        replace = TRUE
+        ),
+    
+    question_1 = 
+      sample(
+        rep(c(1:4)),
+        size = 200,
+        replace = T
+      ),
+    
+    question_2 = 
+      sample(
+        rep(c(1:4)),
+        size = 200,
+        replace = T
+      ),
+    
+    question_3 = 
+      sample(
+        rep(c(1:4)),
+        size = 200,
+        replace = T
+      )
+  )
+
+simulated_questionnaire_responses
+
 # 
-
-
-
-
-#### ####
