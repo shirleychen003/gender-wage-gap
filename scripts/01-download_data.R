380006ddc6a1cb4e7ff2fbb6254c100f89ec6e35
shirkey <sshirleyy.chen@mail.utoronto.ca>
2024-02-06 23:15:49 -0500

    put 01-download_data.R file in scripts folder

---
+++ scripts/01-download_data.R
@@ -0,0 +1,17 @@
+#### Preamble ####
+# Purpose: Download data from the American Economic Association
+# Author: Shirley Chen
+# Date: January 2024
+# Contact: sshirleyy.chen@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: none
+
+#### Workspace setup ####
+library(tidyverse)
+library(haven)
+
+#### Acquire data ####
+converted_data <- read_dta("inputs/data/ISSP-Gender-Norms-2002.dta")
+
+#### Save data as CSV file ####
+write_csv(converted_data, "inputs/data/raw_data.csv")
