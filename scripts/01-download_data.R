8518398e869e9552633373b40befc0ab15fbaab4
Jessica <jessicajwooim@gmail.com>
2024-02-13 14:41:45 -0500

    fixing this idk

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -15,10 +15,6 @@ converted_2002_data <- read_dta("inputs/data/ISSP-Gender-Norms-2002.dta")
 converted_2012_data <- read_dta("inputs/data/ISSP-Gender-Norms-2012.dta")
 
 #### Save data as CSV file ####
-<<<<<<< HEAD
-write_csv(converted_data, "inputs/data/raw_data.csv")
-=======
 write_csv(converted_2002_data, "inputs/data/raw_data_2002.csv")
 write_csv(converted_2012_data, "inputs/data/raw_data_2012.csv")
 
->>>>>>> 784fa946f031ebf6f1db88a6b2f9fa0c742d7b2f
